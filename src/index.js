bfb6d2db92f819059e8f1fe5216181b1b5a01377
SebastianDarie <s.d.01.2002@gmail.com>
2020-08-31 17:02:41 +0300

    add main functionality

--- src/index.js
+++ src/index.js
@@ -1,22 +1,52 @@
-let skycons = new Skycons({ color: '#146EB4' })
-// on Android, a nasty hack is needed: {"resizeClear": true}
+let windIcon = new Skycons({ color: '#65863A' })
+let tempIcon = new Skycons({ color: '#146EB4' })
+let humIcon = new Skycons({ color: '#CC2027' })
 
-// you can add a canvas by it's ID...
-skycons.add('icon1', Skycons.PARTLY_CLOUDY_DAY)
+windIcon.set('wind', 'wind')
+tempIcon.set('temp', 'partly-cloudy-day')
+humIcon.set('hum', 'sleet')
 
-// ...or by the canvas DOM element itself.
-//skycons.add(document.getElementById('icon2'), Skycons.RAIN)
+windIcon.play()
+tempIcon.play()
+humIcon.play()
 
-// if you're using the Forecast API, you can also supply
-// strings: "partly-cloudy-day" or "rain".
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const humidityElement = document.querySelector('[data-humidity]')
+const windElement = document.querySelector('[data-wind]')
+const img = document.querySelector('img')
 
-// start animation!
-skycons.play()
+const search = document.querySelector('.city-search-button')
 
-// you can also halt animation with skycons.pause()
+search.addEventListener('click', fetchWeather)
 
-// want to change the icon? no problem:
-//skycons.set('icon1', Skycons.PARTLY_CLOUDY_NIGHT)
+async function fetchWeather(e) {
+	const city = e.target.parentNode.parentNode.childNodes[1].value
 
-// want to remove one altogether? no problem:
-//skycons.remove('icon2')
+	try {
+		const res = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=8ab539598f978d8393e18c5fad7687f0`
+		)
+		const data = await res.json()
+
+		setWeather(data)
+
+		console.log(data)
+	} catch (err) {
+		alert('You have entered an invalid location or something went wrong')
+	}
+}
+
+function setWeather(data) {
+	locationElement.textContent = data.name + ', ' + data.sys.country
+	statusElement.textContent =
+		data.weather[0].main + ' - ' + data.weather[0].description
+	temperatureElement.textContent = data.main.temp + ' °C'
+	humidityElement.textContent = data.main.humidity + '%'
+	windElement.textContent = data.wind.speed + ' km/h'
+
+	const iconcode = data.weather[0].icon
+	const iconurl = `http://openweathermap.org/img/wn/${iconcode}@2x.png`
+	img.setAttribute('src', iconurl)
+}
